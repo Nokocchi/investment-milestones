999b867dd34ceaac11be8c503ec61e37574b2d3f
Nokocchi <59347338+Nokocchi@users.noreply.github.com>
2025-01-06 14:48:24 +0100

    Removed unused plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import Icons from 'unplugin-icons/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/investment-milestones/',
-  plugins: [svelte(), Icons({ compiler: 'svelte'})],
+  plugins: [svelte()],
 })
