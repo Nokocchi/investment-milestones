5fc4db7d2e7758ebb72ae52a9e234cf4ba56f91f
Nokocchi <59347338+Nokocchi@users.noreply.github.com>
2025-01-07 23:24:46 +0100

    Some kind of data!

---
+++ src/lib/Input.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    let { label, value = $bindable() } = $props();
+</script>
+
+<div class="input-with-label">
+    <label for={"input-" + label}>{label}</label>
+
+    <input id={"input-" + label} bind:value />
+</div>
+
+<style>
+    .input-with-label {
+      display: flex;
+      flex-direction: column;
+      margin-right: 25px;
+    }
+  </style>
